70ac142e87e4029f13988c0a8b17d8eda916b5b5
ITPAT <itpat@ITPATs-MacBook-Air.local>
2023-07-06 05:44:43 +0700

    add cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use(cors({origin: ["http://127.0.0.1:5173"],credentials:true}))
+app.use(cors({origin: ["http://127.0.0.1:5173","http://localhost:5173" ],credentials:true}))
 app.use(cookieparser())
 app.use(fileUpload({
   createParentPath:true,
