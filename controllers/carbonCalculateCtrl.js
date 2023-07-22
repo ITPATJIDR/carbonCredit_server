e196778a153887997326eb46a52c5af856cd96df
Nextradigi <nextradigi@gmail.com>
2023-07-22 19:30:19 +0700

    fix code

--- controllers/carbonCalculateCtrl.js
+++ controllers/carbonCalculateCtrl.js
@@ -1,5 +1,9 @@
+const Template_1 = require('../templates/templates_1')
 const connection = require('../utils/database')
 const axios = require("axios")
+const fs = require('fs');
+const pdfMake = require('pdfmake');
+const { genNumber } = require('../utils/encode');
 
 const headers = {
 	Authorization: `Bearer ${process.env.CC_KEY}`
@@ -110,6 +114,73 @@ const CarbonCalculate = {
 	calculateFood: async (req,res) =>{
 		try{
 				
+		}catch(err){
+			res.status(200).json({ status: 500, message: "Internal Server Error" });
+		}
+	},
+	purchaseOffset: async (req,res) =>{
+		try{
+			const { offset, id } = req.body
+			const getUser_sql = "SELECT * from users WHERE id = ?"
+			const getUser_data = [id]
+			const fonts = {
+				Roboto: {
+					normal: 'assets/fonts/Roboto-Regular.ttf',
+				},
+			}
+			await connection.query(getUser_sql, getUser_data, async (err, result) => {
+				if(err){
+					res.status(200).json({ status: 400, message: "Purchase Failed" });
+				}else{
+					const {compensate_CC, growth_a_tree, name, surname} = result[0]
+					const fullname = name + " " + surname
+					const new_compensate_CC = Math.ceil(Number(compensate_CC) + Math.ceil(offset / 15))
+					const new_growth_a_tree = Math.ceil(Number(growth_a_tree) + Math.ceil(offset / 15))
+					const newData = {
+						compensate_CC: new_compensate_CC,
+						growth_a_tree: new_growth_a_tree
+					}
+					const updatePurchase_sql = "UPDATE users SET ? WHERE id = ?"
+					const updatePurchase_data = [newData, id]
+					await connection.query(updatePurchase_sql, updatePurchase_data, async (err, result) => {
+						if (err) {
+							res.status(200).json({ status: 400, message: "Purchase Failed" });
+						} else {
+							const preCreateCertificate = Template_1(fullname, offset)
+							const printer = new pdfMake(fonts)
+							const pdfDoc = printer.createPdfKitDocument(preCreateCertificate)
+							const fileName = await "CC_" + genNumber()
+							const filePath = `certificates/${fileName}.pdf`
+							const writeStream = fs.createWriteStream(filePath);
+							const createCertificate_sql = "INSERT INTO certificate_list (userId, cert_path) VALUES (?, ?) "
+							const createCertificate_data = [id, filePath]
+							await connection.query(createCertificate_sql, createCertificate_data, (err, result) => {
+								if (err) {
+									res.status(200).json({ status: 200, message: "Purchase Success" });
+								}
+							})
+							pdfDoc.pipe(writeStream);
+							pdfDoc.end();
+							res.status(200).json({ status: 200, message: "Purchase Success" });
+						}
+					})
+					
+
+				}
+			})
+		}catch(err){
+			res.status(200).json({ status: 500, message: "Internal Server Error" });
+		}
+	},
+	downloadCertificate: async (req, res) => {
+		try{
+			const {cert_path} = req.body
+			const fileName = cert_path.split("/")
+			res.download(cert_path, fileName[1], (err) => { 
+					if(err){
+					console.log('Error downloading file:', err.message);
+				}
+			})
 		}catch(err){
 			res.status(200).json({ status: 500, message: "Internal Server Error" });
 		}
