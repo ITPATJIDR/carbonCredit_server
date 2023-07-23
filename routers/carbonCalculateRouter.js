809bc0e9c9f2ac6be9669f1b22b5b18d1c333982
Papatsiri2002 <91860184+Papatsiri2002@users.noreply.github.com>
2023-07-23 22:44:02 +0700

    pull

--- routers/carbonCalculateRouter.js
+++ routers/carbonCalculateRouter.js
@@ -6,6 +6,7 @@ Router.get("/getAllCarbonFoodList",CarbonCalculate.getAllCarbonFoodList)
 Router.post("/authAPI",CarbonCalculate.authCarbonInterfaceApi)
 Router.get("/getVehicle",CarbonCalculate.getVehicle)
 Router.post("/calVehicle",CarbonCalculate.calculateVehicle)
+Router.post("/calFood",CarbonCalculate.calculateFood)
 Router.post("/purchase",CarbonCalculate.purchaseOffset)
 Router.post("/downloadCertificate", CarbonCalculate.downloadCertificate)
 
