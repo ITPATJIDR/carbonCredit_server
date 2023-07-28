49af671bb4e57b16e2fe670e70bf5a2fd916dee0
Nextradigi <nextradigi@gmail.com>
2023-07-28 15:27:44 +0700

    test deploy

--- controllers/carbonCalculateCtrl.js
+++ controllers/carbonCalculateCtrl.js
@@ -350,6 +350,7 @@ const CarbonCalculate = {
 				res.status(200).json({ status: 403, message: "Invalid Api key" });
 			}
 		}catch(err){
+
 			res.status(200).json({ status: 500, message: "Internal Server Error" });
 		}
 	},
