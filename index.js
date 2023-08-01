9601bec42221420a3f7e02c03a188d218bf3caf0
ITPAT <nextradigi@gmail.com>
2023-08-01 17:19:22 +0700

    fix cors

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ app.use("/certificates", express.static("certificates"))
 app.use("/uploads", express.static("uploads"))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("I'm Greenie...")
 })
