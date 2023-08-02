2b93b47a73466d0fd87abb85b99f934a4a369980
ITPAT <nextradigi@gmail.com>
2023-08-02 11:10:08 +0700

    fix cookie

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -53,7 +53,7 @@ const userCtrl = {
 						if (checkPassword) {
 							const refreshtoken = await jwtRefreshToken(result[0])
 							res.cookie("refreshtoken", refreshtoken, {
-								domain:".azurewebsites.net",
+								domain:"azurewebsites.net",
 								path: "/user/getRefreshToken",
 								maxAge: 8 * 24 * 60 * 60 * 1000, // 7 days
 								httpOnly: true,
